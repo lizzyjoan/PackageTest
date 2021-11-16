522b854b0cdb2f19c63bc3de715fa88a1a4ae85f
lizzyjoan <lizzyr@uab.edu>
2021-11-16 11:03:48 -0600

    Initial commit

--- R/Scripttest.R
+++ R/Scripttest.R
@@ -5,6 +5,12 @@ usethis::use_git()
 usethis::use_github()
 
 devtools::check()
+#Error: This repo already has an 'origin' remote, with value 'https://github.com/lizzyjoan/PackageTest.git'.
+#You can remove this setting with:
+#  `usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)`
+usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
+#try again
+devtools::check()
 
 #define function
 use_r("test_id")
